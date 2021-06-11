42020dea43be4918e75156ef5bd4621a9fc1dbe3
wangzewei <120668292@qq.com>
2021-06-11 11:52:42 +0800

    refactor(lemon-gin-web-framework)：not use gorm soft delete

--- model/Model.go
+++ model/Model.go
@@ -1,8 +1,7 @@
 package model
 
 type Model struct {
-	ID        uint `gorm:"primarykey;AUTO_INCREMENT" json:"id"` // 主键ID
+	ID        uint `gorm:"primaryKey;autoIncrement" json:"id"` // 主键ID
 	CreatedAt int  `json:"created_at"`           // 创建时间
 	UpdatedAt int  `json:"updated_at"`           // 更新时间
-	DeletedAt int  `gorm:"index" json:"-"`       // 删除时间
 }
